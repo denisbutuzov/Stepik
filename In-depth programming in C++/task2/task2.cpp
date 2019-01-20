12ee0e9ab813360bb0132947f4c465881d46c6e6
Denis Butuzov <qwaswedo@gmail.com>
2019-01-20 20:29:06 +0700

    Fixed task2

--- In-depth programming in C++/task2/task2.cpp
+++ In-depth programming in C++/task2/task2.cpp
@@ -2,75 +2,103 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <list>
 #include <utility>
 #include <regex>
 
-std::pair<int, std::vector<double>> parse_string(std::string &&string)
+void replace_all_substrings_in_string(std::string &string, 
+				      const std::string &old_substring,
+				      const std::string &new_substring)
 {
-	std::string delimiter(" ");
-	std::string del("\\n");
 	std::size_t pos;
 
-	double number = -1;
-	std::vector<double> parsed_sequence;
-	
-	while((pos = string.find(del)) != std::string::npos)
+	while((pos = string.find(old_substring)) != std::string::npos)
 	{
-		string.replace(pos, del.length(), delimiter);
+		string.replace(pos, old_substring.length(), new_substring);
 	}
+}
+
+std::list<std::string> split_string(std::string &&string)
+{
+	std::string delimiter(" ");
+	std::size_t pos;
+	std::list<std::string> splitted_string;
 
 	while((pos = string.find(delimiter)) != std::string::npos)
 	{
-		if(number == -1)
-		{
-			number = std::stod(string.substr(0, pos));
-			if(number - static_cast<int>(number) != 0)
-			{
-				number = 0;
-			}
-		}
-		else
-		{
-			parsed_sequence.push_back(std::stod(string.substr(0, pos)));
-		}
+		splitted_string.push_back(string.substr(0, pos));
 		string.erase(0, pos + delimiter.length());
 	}
-	parsed_sequence.push_back(std::stod(string));
+	splitted_string.push_back(string);
 
-	return std::move(std::pair<int, std::vector<double>> {static_cast<int>(number), parsed_sequence});
+	return std::move(splitted_string);
+}
+
+std::tuple<bool, int, std::vector<double>> parse_string(std::list<std::string> &&string_list)
+{
+	bool valid_string = true;
+	int number_elements;
+	std::vector<double> elements;
+	
+	if(std::regex_match(string_list.front(), std::regex("^\\+{0,1}\\d+$")))
+	{
+		number_elements = std::stoi(string_list.front());
+		string_list.pop_front();
+
+		while(string_list.size() > 0)
+		{
+			if(std::regex_match(string_list.front(), std::regex("^[-+]?[0-9]*[.,]?[0-9]+(?:[eE][-+]?[0-9]+)?$")))
+			{
+				elements.push_back(std::stod(string_list.front()));
+				string_list.pop_front();
+			}
+			else
+			{
+				valid_string = false;
+				break;
+			}
+		}
+
+	}
+	else
+	{
+		valid_string = false;
+	}
+
+	return std::move(std::make_tuple(valid_string, number_elements, elements));
 }
 
 std::pair<int, int> find_sequence(std::vector<double>::const_iterator begin,
                                   std::vector<double>::const_iterator end)
 {
-	int temp_count = 1;
+	int temp_max_count = 1;
 	int max_count = 0;
-	auto temp_pos = begin;
-	decltype(temp_pos) max_pos;
+	int temp_max_pos = 0;
+	int max_pos = 0;
 	for(auto it = begin; it != (end - 1); it++)
 	{
 		if(*it <= *(it + 1))
 		{
-			++temp_count;
+			++temp_max_count;
 		}
 		else
 		{
-			if(max_count < temp_count)
+			if(max_count < temp_max_count)
 			{
-				max_count = temp_count;
-				max_pos = temp_pos;
+				max_count = temp_max_count;
+				max_pos = temp_max_pos;
 			}
-			temp_count = 1;
-			temp_pos = std::find(max_pos, end, *it) + 1;
+			temp_max_count = 1;
+			temp_max_pos = std::distance(begin, it) + 1;
 		}
 	}
-	if(max_count < temp_count)
+	if(max_count < temp_max_count)
 	{
-		max_count = temp_count;
-		max_pos = temp_pos;
+		max_count = temp_max_count;
+		max_pos = temp_max_pos;
 	}
 
-	return std::move(std::pair<int, int> {max_count, max_pos - begin + 1});
+	return std::move(std::make_pair(max_count, max_pos + 1));
 }
 
 int main(int argc, char *argv[])
@@ -78,15 +106,23 @@ int main(int argc, char *argv[])
 	std::string str;
 	std::getline(std::cin, str);
 
-	auto parsed_string = parse_string(std::move(str));
-	if(parsed_string.first < 2 || parsed_string.second.size() < parsed_string.first)
+	replace_all_substrings_in_string(str, "\\n", " ");
+	std::list<std::string> splitted_string = split_string(std::move(str));
+
+
+	bool valid;
+	int number;
+	std::vector<double> elements;
+
+	std::tie(valid, number, elements) = parse_string(std::move(splitted_string));
+	if(!valid || number < 2 || number > elements.size())
 	{
 		std::cout << "[error]" << std::endl;
 	}
 	else
 	{
-		auto sequence = find_sequence(parsed_string.second.cbegin(),
-                                              parsed_string.second.cbegin() + parsed_string.first);
+		auto sequence = find_sequence(elements.cbegin(),
+                                              elements.cbegin() + number);
 
 		if(sequence.first < 2)
 		{
